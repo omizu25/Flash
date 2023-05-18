f5066258ea995ce3fc11d359c55b84492535f35c
Katsuki Mizuki <m.z.k.1125@ezweb.ne.jp>
2023-05-18 21:56:00 +0900

    基盤完成

---
+++ application.cpp
@@ -0,0 +1,242 @@
+//**************************************************
+// 
+// application.cpp
+// Author  : katsuki mizuki
+// 
+//**************************************************
+
+//==================================================
+// インクルード
+//==================================================
+#include "application.h"
+#include "renderer.h"
+#include "input.h"
+#include "object.h"
+#include "texture.h"
+#include "mode.h"
+#include "fade.h"
+
+//==================================================
+// 定義
+//==================================================
+namespace
+{
+const bool FULL_SCREEN = false;	// フルスクリーンにするかどうか
+const CMode::EMode START_MODE = CMode::MODE_TITLE;	// 最初のモード
+}
+
+//==================================================
+// 静的メンバ変数
+//==================================================
+CApplication* CApplication::m_pApplication = nullptr;
+
+//--------------------------------------------------
+// インスタンスの取得
+//--------------------------------------------------
+CApplication* CApplication::GetInstance()
+{
+	if (m_pApplication == nullptr)
+	{// nullチェック
+		m_pApplication = new CApplication;
+	}
+
+	return m_pApplication;
+}
+
+//--------------------------------------------------
+// デフォルトコンストラクタ
+//--------------------------------------------------
+CApplication::CApplication() :
+	m_pRenderer(nullptr),
+	m_pTexture(nullptr),
+	m_pMode(nullptr),
+	m_pFade(nullptr)
+{
+}
+
+//--------------------------------------------------
+// デストラクタ
+//--------------------------------------------------
+CApplication::~CApplication()
+{
+	assert(m_pFade == nullptr);
+	assert(m_pMode == nullptr);
+	assert(m_pTexture == nullptr);
+	assert(m_pRenderer == nullptr);
+}
+
+//--------------------------------------------------
+// 初期化
+//--------------------------------------------------
+HRESULT CApplication::Init(HINSTANCE hInstance, HWND hWnd)
+{
+	{// レンダラー
+		m_pRenderer = new CRenderer;
+
+		// nullチェック
+		assert(m_pRenderer != nullptr);
+
+		if (FAILED(m_pRenderer->Init(hWnd, !FULL_SCREEN)))
+		{// 初期化
+			return S_FALSE;
+		}
+	}
+
+	{// 入力
+		CInput::Create();
+
+		if (FAILED(CInput::GetKey()->Init(hInstance, hWnd)))
+		{// 初期化
+			return E_FAIL;
+		}
+	}
+
+	{// テクスチャ
+		m_pTexture = new CTexture;
+
+		// nullチェック
+		assert(m_pTexture != nullptr);
+	}
+
+	{// フェード
+		m_pFade = CFade::Create();
+
+		// nullチェック
+		assert(m_pFade != nullptr);
+
+		// 初期化
+		m_pFade->Init();
+	}
+
+	m_mode = START_MODE;
+
+	// モードの設定
+	m_pFade->ChangeMode(m_mode);
+
+	// ランダムの初期化
+	srand((unsigned int)time(NULL));
+
+	return S_OK;
+}
+
+//--------------------------------------------------
+// 終了
+//--------------------------------------------------
+void CApplication::Uninit()
+{
+	// 全ての解放
+	CObject::ReleaseAll();
+
+	m_pMode = nullptr;
+
+	if (m_pFade != nullptr)
+	{// nullチェック
+		m_pFade->Uninit();
+		m_pFade = nullptr;
+	}
+
+	if (m_pTexture != nullptr)
+	{// nullチェック
+		m_pTexture->ReleaseAll();
+		delete m_pTexture;
+		m_pTexture = nullptr;
+	}
+
+	// 入力処理の終了
+	CInput::GetKey()->Uninit();
+
+	if (m_pRenderer != nullptr)
+	{// nullチェック
+		m_pRenderer->Uninit();
+		delete m_pRenderer;
+		m_pRenderer = nullptr;
+	}
+}
+
+//--------------------------------------------------
+// 更新
+//--------------------------------------------------
+void CApplication::Update()
+{
+	// 入力処理の更新
+	CInput::GetKey()->Update();
+
+	if (m_pRenderer != nullptr)
+	{// nullチェック
+		// 更新
+		m_pRenderer->Update();
+	}
+}
+
+//--------------------------------------------------
+// 描画
+//--------------------------------------------------
+void CApplication::Draw()
+{
+	if (m_pRenderer != nullptr)
+	{// nullチェック
+		// 描画処理
+		m_pRenderer->Draw();
+	}
+}
+
+//--------------------------------------------------
+// モードの設定
+//--------------------------------------------------
+CMode::EMode CApplication::GetModeEnum()
+{
+	return m_mode;
+}
+//--------------------------------------------------
+// モードの設定
+//--------------------------------------------------
+void CApplication::SetMode(CMode::EMode mode)
+{
+	m_mode = mode;
+
+	// 全ての解放
+	CObject::ReleaseAll();
+
+	// モードの生成
+	m_pMode = CMode::Create(mode);
+}
+
+//--------------------------------------------------
+// デバイスの取得
+//--------------------------------------------------
+LPDIRECT3DDEVICE9 CApplication::GetDevice()
+{
+	return m_pRenderer->GetDevice();
+}
+
+//--------------------------------------------------
+// レンダラーの取得
+//--------------------------------------------------
+CRenderer* CApplication::GetRenderer()
+{
+	return m_pRenderer;
+}
+
+//--------------------------------------------------
+// テクスチャの取得
+//--------------------------------------------------
+CTexture* CApplication::GetTexture()
+{
+	return m_pTexture;
+}
+
+//--------------------------------------------------
+// モードの取得
+//--------------------------------------------------
+CMode* CApplication::GetMode()
+{
+	return m_pMode;
+}
+
+//--------------------------------------------------
+// フェードの取得
+//--------------------------------------------------
+CFade* CApplication::GetFade()
+{
+	return m_pFade;
+}
---
+++ title.cpp
@@ -0,0 +1,129 @@
+//**************************************************
+// 
+// title.cpp
+// Author  : katsuki mizuki
+// 
+//**************************************************
+
+//==================================================
+// インクルード
+//==================================================
+#include "title.h"
+#include "input.h"
+#include "application.h"
+#include "menu.h"
+#include "fade.h"
+#include "object2D.h"
+
+//==================================================
+// 定義
+//==================================================
+namespace
+{
+}
+
+//--------------------------------------------------
+// デフォルトコンストラクタ
+//--------------------------------------------------
+CTitle::CTitle() :
+	m_pMenu(nullptr)
+{
+}
+
+//--------------------------------------------------
+// デストラクタ
+//--------------------------------------------------
+CTitle::~CTitle()
+{
+	assert(m_pMenu == nullptr);
+}
+
+//--------------------------------------------------
+// 初期化
+//--------------------------------------------------
+void CTitle::Init()
+{
+	{// 背景
+		CObject2D* pObj = CObject2D::Create();
+
+		float width = (float)CApplication::SCREEN_WIDTH;
+		float height = (float)CApplication::SCREEN_HEIGHT;
+
+		// 位置の設定
+		pObj->SetPos(D3DXVECTOR3(width * 0.5f, height * 0.5f, 0.0f));
+
+		// サイズの設定
+		pObj->SetSize(D3DXVECTOR3(width, height, 0.0f));
+
+		// 色の設定
+		pObj->SetCol(D3DXCOLOR(0.75f, 0.75f, 0.75f, 1.0f));
+
+		// テクスチャの設定
+		//pObj->SetTexture(CTexture::LABEL_TITLE_BG);
+	}
+
+	{// メニュー
+		D3DXVECTOR3 pos = D3DXVECTOR3((float)CApplication::SCREEN_WIDTH * 0.5f, (float)CApplication::SCREEN_HEIGHT * 0.75f, 0.0f);
+		D3DXVECTOR3 size = D3DXVECTOR3(650.0f, 100.0f, 0.0f);
+
+		// 生成
+		m_pMenu = CMenu::Create(pos, size, 1, 50.0f);
+
+		// 枠の設定
+		m_pMenu->SetFrame(pos, D3DXVECTOR3((float)CApplication::SCREEN_WIDTH, 200.0f, 0.0f), D3DXCOLOR(0.0f, 0.0f, 0.0f, 0.5f));
+
+		// テクスチャの設定
+		m_pMenu->SetTexture(0, CTexture::LABEL_PressEnter);
+	}
+}
+
+//--------------------------------------------------
+// 終了
+//--------------------------------------------------
+void CTitle::Uninit()
+{
+	if (m_pMenu != nullptr)
+	{// nullチェック
+		m_pMenu = nullptr;
+	}
+
+	// 解放
+	Release();
+}
+
+//--------------------------------------------------
+// 更新
+//--------------------------------------------------
+void CTitle::Update()
+{
+	// 入力
+	Input();
+}
+
+//--------------------------------------------------
+// 描画
+//--------------------------------------------------
+void CTitle::Draw()
+{
+}
+
+//--------------------------------------------------
+// 入力
+//--------------------------------------------------
+void CTitle::Input()
+{
+	if (m_pMenu == nullptr)
+	{// nullチェック
+		return;
+	}
+
+	int select = m_pMenu->Select();
+
+	if (select == -1)
+	{// 入力されていない
+		return;
+	}
+
+	// モードの変更
+	CApplication::GetInstance()->GetFade()->ChangeMode(EMode::MODE_GAME);
+}
